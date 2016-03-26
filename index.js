82cd9055f7c726eab4e1aee01c35dec74e4e39fe
James Talmage <james@talmage.io>
2016-03-26 18:31:16 -0400

    use pify over fs-promise
    This allows nyc coverage to work

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var template = require("lodash.template")
-var readFile = require("fs-promise").readFile
+var fs = require("fs");
+var pify = require('pify');
+
+function readFile(filePath) {
+  return pify(fs.readFile)(filePath, 'utf8');
+}
+
 /**
  * Read file and return a string with given vars using lodash.template
  */
 module.exports = function(filePath, templateVars) {
-  return readFile(filePath, { encode: "utf-8" })
+  return readFile(filePath)
     .then(function(content) {
       return template(content)(templateVars)
-    })
+    });
 }
